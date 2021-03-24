113dc46583787859396a3db6a8fd7d0592842bfa
gabriellemhobson <ghobson@ucsd.edu>
2021-03-24 18:53:26 -0400

    Corrected norm in convergence ananalysis, now seeing 2nd order convergence as expected.

--- cn_1D_diffusion_iter.py
+++ cn_1D_diffusion_iter.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  1 22:27:14 2021
-
-@author: ghobson
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 """
 A function that can be called to implement Crank-Nicolson (implicit, 2nd order
 in both space and time, as efficient as explicit) to solve the 1D diffusion 
-equation. 
+equation on the domain (0,L). 
 
 Inputs: N: number of grid points
-k: time step
+dt: time step
 L: length of grid
 t: current time step number
-g0: left boundary condition (for the moment, a scalar)
-g1: right boundary condition (for the moment, a scalar)
+g0: left boundary condition
+g1: right boundary condition
 kappa: the diffusion coefficient (constant for now)
 un: previous time step (a vector)
 
@@ -29,7 +21,7 @@ Uses previous time step to find next time step.
 
 """
 
-def cn_1D_diffusion(N,k,L,t,g0,g1,kappa,un):
+def cn_1D_diffusion(N,dt,L,t,g0,g1,kappa,un):
     # importing packages
     import numpy as np 
     import scipy as scipy
@@ -37,35 +29,31 @@ def cn_1D_diffusion(N,k,L,t,g0,g1,kappa,un):
     from time import perf_counter
     
     t1 = perf_counter() # start timing
-    
     h = L/(N-1) # space step size
-    r = k*kappa/(2*h**2) # define r, later used in matrix system
+    r = dt*kappa/(2*h**2) # define r, later used in matrix system
     
-    # set up matrices for A x = b system
-    
-    # A is tridiagonal and sparse, set it up directly, 
-    # this creates a sparse matrix in CSR format
+    # settin up matrices for A x = b system
+    # A is a tridiagonal and sparse matrix in CSR format
     rs = np.zeros(N-1) # for the off diagonals
-    rs[1:-1] += r # for the off diagonals
+    rs[1:-1] += r # add r to interior rows only
     main_diag = np.ones(N) # for the center diagonal
-    main_diag[1:-1] *= (1+2*r) 
+    main_diag[1:-1] *= (1+2*r) # add (1+2*r) to interior rows only
     A = scipy.sparse.diags([main_diag, -rs, -rs],[0,-1,1],format="csr")
-    
+ 
     # set first 'solution' to be the previous time step
     U = un
     # create rhs that we can fill in
     b = np.zeros(N)
-    b[0] = g0(t+k) # meeting bc
-    b[1] = r*(g0(t) + g0(t+k)) + (1-2*r)*U[1] + r*U[2]
+    b[0] = g0(t+dt) # meeting bc
+    b[1] = r*(g0(t) + g0(t+dt)) + (1-2*r)*U[1] + r*U[2]
     # second row to second-to-last row
     for j in range(2,N-2):
         b[j] = r*U[j-1] + (1 - 2*r)*U[j] + r*U[j+1]
-    b[-2] = r*U[-3] + (1-2*r)*U[-2] + r*(g1(t) + g1(t+k))
-    b[-1] = g1(t+k) # meeting bc
+    b[-2] = r*U[-3] + (1-2*r)*U[-2] + r*(g1(t) + g1(t+dt))
+    b[-1] = g1(t+dt) # meeting bc
     # using spsolve: currently A is in CSR form
-    U_out = scipy.sparse.linalg.spsolve(A,b)
+    U_out = spsolve(A,b)
     t2 = perf_counter()
     dt = t2-t1
 #    print('[scipy.sparse.linalg.spsolve] time',('%1.4e'%dt),'(sec)')
-    
     return U_out
--- pde_cn_diff_iter.py
+++ pde_cn_diff_iter.py
@@ -38,7 +38,7 @@ class CN_Diffusion(pb.PDEBase):
         def g1(t):
             return 0 # for now
         
-        #solver.cn_1D_diffusion(N,k,L,t,g0,g1,un)
+        #solver.cn_1D_diffusion(N,dt,L,t,g0,g1,un)
         kappa = 1
         U = solver.cn_1D_diffusion(self.N,self.dt,self.L,self.time,g0,g1,kappa,phi_k)
         print('phi_k norm:', np.linalg.norm(phi_k))
@@ -53,49 +53,72 @@ def test_pde_cn():
         print('solution norm', np.linalg.norm(U))
 
 def test_cn():
-    c = int(2)
-    N = int(128*c) # number of grid points
-    k = 1e-4/c # time step
-    L = float(2) # length of grid
-    nt = int(1000*c) # number of time steps
-    kappa = 1
+    nrm = np.zeros((5))
+    hs = np.zeros((5))
+    for i in range(1,6):
+        #c = int(2*i)
+        N = int(128*2**i) # number of grid points
+        dt = 1e-6 # time step
+        L = float(2) # length of grid
+        nt = int(1) # number of time steps
+        kappa = 1
+        #a = 1
+        
+        def true(x,t):
+            return (1.0/(1.0+4.0*t)**(1.0/2.0))*np.exp(-x**2/(1.0+4.0*t))
+            # the below true solution works on (0,1)
+            # return np.exp(-np.pi**2*t)*np.sin(np.pi*x)+0.1*np.exp(-np.pi**2*1e4*t)*np.sin(100*np.pi*x)
+
+        # define boundary conditions
+        def g0(t):
+            return true(0.0,np.float(t)) 
+            # return 0
     
-    def true(x,t):
-        return (1.0/(1.0+4.0*t)**(1.0/2.0))*np.exp(-x**2/(1.0+4.0*t))
-
-    # define boundary conditions
-    def g0(t):
-        return true(-1.0,np.float(t)) # 2 for other ic
-
-    def g1(t):
-        return true(1.0,np.float(t)) # 1.5 for other ic
+        def g1(t):
+            return true(L,np.float(t))
+            # return 0
+            
+        # define initial condition
+    #    u0 = 0.5*np.exp(-(x-0.5)**2 / (2*(1/8)**2))
+        # trying out a different initial condition
+    #    u0 = 2 - 1.5*x + np.sin(np.pi*x)
     
-    # define initial condition
-#    h = L/(N-1)
-#    x = np.linspace(0,N-1,N)*h
-#    u0 = 0.5*np.exp(-(x-0.5)**2 / (2*(1/8)**2))
-    # trying out a different initial condition
-#    u0 = 2 - 1.5*x + np.sin(np.pi*x)
+        # define initial condition
+        x = np.linspace(0,L,N)
+        u0 = np.exp(-x**2)
+        # u0 = np.sin(np.pi*x) + 0.1*np.sin(100*np.pi*x) # zero bc, (0,1)
+    
+        un = u0
+        t = 0.0
+        for j in range((nt)):
+            un = solver.cn_1D_diffusion(N,dt,L,t,g0,g1,kappa,un)
+            t += dt
+        
+        plt.clf()
+        plt.ylim(0,1)
+        plt.plot(x,un,color='blue')
+        plt.scatter(x,true(x,t),marker='o',color='orange')
+        plt.title(['t =', t])
+        plt.show()
+        
+        hs[i-1] = L/(N-1)
+        # nrm[i-1] = np.linalg.norm(un-true(x,t))
+        nrm[i-1] = np.linalg.norm(un-true(x,t)) * np.sqrt(hs[i-1])
+        print('Norm',('%1.4e'%nrm[i-1]))
+        print('')
 
-    # define initial condition
-    x = np.linspace(-L/2,L/2,N)
-    u0 = np.exp(-x**2)
-
-    un = u0
-    t = 0.0
-    for j in range((nt)):
-        un = solver.cn_1D_diffusion(N,k,L,t,g0,g1,kappa,un)
-        t += k
-    plt.clf()
-    plt.ylim(0,1)
-    plt.plot(x,un,color='blue')
-    plt.scatter(x,true(x,t),marker='o',color='orange')
-    plt.title(['t =', t])
+    print('h',hs)
+    print('nrm',nrm)
+    fig = plt.figure()
+    ax=plt.gca()
+    ax.scatter(hs,nrm,c="blue")
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    m, c = np.polyfit(np.log(hs), np.log(nrm), deg=1)
+    ax.plot(hs, np.exp(m*np.log(hs) + c), color='red') # add reg line
+    ax.grid(b='on')
     plt.show()
-
-    nrm = np.linalg.norm(un-true(x,t))
-    print('Norm',('%1.4e'%nrm))
-
+    print('Slope:',m)
 if __name__ == '__main__':
     test_cn()
 #    test_pde_cn()
