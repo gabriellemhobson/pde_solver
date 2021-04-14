dd0f1305dd496e8deb4c11847e43fa22ac36bff5
gabriellemhobson <ghobson@ucsd.edu>
2021-04-13 20:10:30 -0400

    Adding 2D diffusion code which contains basic_demo() and a convergence check.

--- cn_2D_adv_diff.py
+++ cn_2D_adv_diff.py
@@ -24,7 +24,12 @@ Uses previous time step to find next time step.
 
 """
 
-def cn_2D_diffusion(N,dt,L,t,g0,g1,vx,vy,kx,ky,un):
+def cn_2D_diffusion(N,dt,L,t,g0,g1,h0,h1,vx,vy,kx,ky,un):
+    """
+    Right now this function handles boundary conditions the way it is done in
+    the atmospheric modeling textbook, using the previous time step values at
+    the boundary. 
+    """
     # importing packages
     import numpy as np 
     import scipy as scipy
@@ -74,14 +79,15 @@ def cn_2D_diffusion(N,dt,L,t,g0,g1,vx,vy,kx,ky,un):
     # in y
     lhs_lower = A(vy,ky)*np.ones(N)
     lhs_diag = B(ky)*np.ones(N+1)
-    lhsdiagp1 = D(vy,ky)*np.ones(N)
-    rhsdiagm1 = E(vy,ky)*np.ones(N)
-    rhsdiag = F(ky)*np.ones(N+1)
-    rhsdiagp1 = G(vy,ky)*np.ones(N)
-    lhsY = np.diag(lhs_lower,-1) + np.diag(lhs_diag,0) + np.diag(lhsdiagp1,1)
-    rhsY = np.diag(rhsdiagm1,-1) + np.diag(rhsdiag,0) + np.diag(rhsdiagp1,1)
-    lhsY[0,0] += A(vy,ky) # applying BCs
-    lhsY[-1,-1] += D(vy,ky) # applying BCs
+    lhs_upper = D(vy,ky)*np.ones(N)
+    lhsY = scipy.sparse.diags([lhs_diag,lhs_lower,lhs_upper],[0,-1,1],format="csr")
+    lhsY[0,0] += A(vy,kx) # applying BCs
+    lhsY[-1,-1] += D(vy,kx) # applying BCs 
+    
+    rhs_lower = E(vy,ky)*np.ones(N)
+    rhs_diag = F(ky)*np.ones(N+1)
+    rhs_upper = G(vy,ky)*np.ones(N)
+    rhsY = scipy.sparse.diags([rhs_diag,rhs_lower,rhs_upper],[0,-1,1],format="csr")
     rhsY[0,0] += E(vy,ky) # applying BCs
     rhsY[-1,-1] += G(vy,ky) # applying BCs
     for x in range(N+1):
@@ -93,3 +99,217 @@ def cn_2D_diffusion(N,dt,L,t,g0,g1,vx,vy,kx,ky,un):
     dt = t2-t1
     print('[scipy.sparse.linalg.spsolve] time',('%1.4e'%dt),'(sec)')
     return U_out
+
+def cn_2D_diffusion_v2(N,dt,L,t,g0,g1,h0,h1,vx,vy,kx,ky,un):
+    # importing packages
+    import numpy as np 
+    import scipy as scipy
+    from scipy.sparse.linalg import spsolve
+    from time import perf_counter
+    
+    t1 = perf_counter() # start timing
+    
+    h = L/(N-1) # space step size
+    xs = np.linspace(-L/2,L/2,N)
+    ys = np.linspace(-L/2,L/2,N)
+    XX, YY = np.meshgrid(xs,ys)
+    
+    def A(v,K):
+        return -dt/2*(v/2/h + K/h**2)
+    def B(K):
+        return 1 + dt*K/h**2
+    def D(v,K):
+        return dt/2*(v/2/h - K/h**2)
+    def E(v,K):
+        return dt/2*(v/2/h + K/h**2)
+    def F(K): 
+        return 1 - dt*K/h**2
+    def G(v,K):
+        return dt/2*(-v/2/h + K/h**2)
+    
+    X = np.zeros((N+1,N+1))
+    Y = np.zeros((N+1,N+1))
+    
+    lhs_lower = A(vx,kx)*np.ones(N)
+    lhs_diag = B(kx)*np.ones(N+1)
+    lhs_upper = D(vx,kx)*np.ones(N)
+    lhsX = scipy.sparse.diags([lhs_diag,lhs_lower,lhs_upper],[0,-1,1],format="csr")
+    lhsX[0,0] += A(vx,kx) # applying BCs
+    lhsX[-1,-1] += D(vx,kx) # applying BCs 
+    
+    rhs_lower = E(vx,kx)*np.ones(N)
+    rhs_diag = F(kx)*np.ones(N+1)
+    rhs_upper = G(vx,kx)*np.ones(N)
+    rhsX = scipy.sparse.diags([rhs_diag,rhs_lower,rhs_upper],[0,-1,1],format="csr")
+    rhsX[0,0] += E(vx,kx) # applying BCs here but also in loop
+    rhsX[-1,-1] += G(vx,kx) # applying BCs here but also in loop
+    
+    for y in range(N+1):  
+        # applying boundary conditions strictly, so we are using true values
+        # rather than values at previous time step
+        un[y,0] = h0(y,t)
+        un[y,-1] = h1(y,t)
+        X[y,:] = spsolve(lhsX,rhsX.dot(un[y,:]))
+
+    # in y
+    lhs_lower = A(vy,ky)*np.ones(N)
+    lhs_diag = B(ky)*np.ones(N+1)
+    lhs_upper = D(vy,ky)*np.ones(N)
+    lhsY = scipy.sparse.diags([lhs_diag,lhs_lower,lhs_upper],[0,-1,1],format="csr")
+    lhsY[0,0] += A(vy,kx) # applying BCs
+    lhsY[-1,-1] += D(vy,kx) # applying BCs 
+    
+    rhs_lower = E(vy,ky)*np.ones(N)
+    rhs_diag = F(ky)*np.ones(N+1)
+    rhs_upper = G(vy,ky)*np.ones(N)
+    rhsY = scipy.sparse.diags([rhs_diag,rhs_lower,rhs_upper],[0,-1,1],format="csr")
+    rhsY[0,0] += E(vy,ky) # applying BCs
+    rhsY[-1,-1] += G(vy,ky) # applying BCs
+    for x in range(N+1):
+        # applying boundary conditions strictly, so we are using true values
+        # rather than values at previous time step
+        X[0,x] = g0(x,t)
+        X[-1,x] = g1(x,t)
+        Y[:,x] = spsolve(lhsY,rhsY.dot(X[:,x]))
+    
+    U_out = Y
+
+    t2 = perf_counter()
+    dt = t2-t1
+    print('[scipy.sparse.linalg.spsolve] time',('%1.4e'%dt),'(sec)')
+    return U_out
+
+def cn_2D_diffusion_alt(N,dt,L,t,g0,g1,h0,h1,vx,vy,kx,ky,un,alt):
+    # importing packages
+    import numpy as np 
+    import scipy as scipy
+    from scipy.sparse.linalg import spsolve
+    from time import perf_counter
+    
+    t1 = perf_counter() # start timing
+    
+    h = L/(N-1) # space step size
+    xs = np.linspace(-L/2,L/2,N)
+    ys = np.linspace(-L/2,L/2,N)
+    XX, YY = np.meshgrid(xs,ys)
+    
+    def A(v,K):
+        return -dt/2*(v/2/h + K/h**2)
+    def B(K):
+        return 1 + dt*K/h**2
+    def D(v,K):
+        return dt/2*(v/2/h - K/h**2)
+    def E(v,K):
+        return dt/2*(v/2/h + K/h**2)
+    def F(K): 
+        return 1 - dt*K/h**2
+    def G(v,K):
+        return dt/2*(-v/2/h + K/h**2)
+    
+    UX = np.zeros((N+1,N+1))
+    UY = np.zeros((N+1,N+1))
+    
+    if alt == 1:
+        # want to do y then x
+        lhs_lower = A(vx,kx)*np.ones(N)
+        lhs_diag = B(kx)*np.ones(N+1)
+        lhs_upper = D(vx,kx)*np.ones(N)
+        lhsX = scipy.sparse.diags([lhs_diag,lhs_lower,lhs_upper],[0,-1,1],format="csr")
+        
+        rhs_lower = E(vx,kx)*np.ones(N)
+        rhs_diag = F(kx)*np.ones(N+1)
+        rhs_upper = G(vx,kx)*np.ones(N)
+        
+        for y in range(N+1):  
+            # applying boundary conditions strictly, so we are using true values
+            # rather than values at previous time step
+            rhs_diag[0] += E(vx,kx)*h0(y,t-dt) - A(vx,kx)*h0(y,t)# applying BCs here 
+            rhs_upper[0] += E(vx,kx)*h0(y,t-dt) - A(vx,kx)*h0(y,t)
+            rhs_diag[-1] += G(vx,kx)*h1(y,t-dt) - D(vx,kx)*h1(y,t)# applying BCs here 
+            rhs_lower[-1] += G(vx,kx)*h1(y,t-dt) - D(vx,kx)*h1(y,t)#
+            rhsX = scipy.sparse.diags([rhs_diag,rhs_lower,rhs_upper],[0,-1,1],format="csr")
+            UX[y,:] = spsolve(lhsX,rhsX.dot(un[y,1:-1]))
+        
+        # in y
+        lhs_lower = A(vy,ky)*np.ones(N)
+        lhs_diag = B(ky)*np.ones(N+1)
+        lhs_upper = D(vy,ky)*np.ones(N)
+        lhsY = scipy.sparse.diags([lhs_diag,lhs_lower,lhs_upper],[0,-1,1],format="csr")
+        
+        rhs_lower = E(vy,ky)*np.ones(N)
+        rhs_diag = F(ky)*np.ones(N+1)
+        rhs_upper = G(vy,ky)*np.ones(N)
+        for x in range(N+1):
+            # applying boundary conditions strictly, so we are using true values
+            # rather than values at previous time step
+            rhs_diag[0] += E(vx,kx)*g0(x,t-dt) - A(vx,kx)*g0(x,t)# applying BCs here but also in loop
+            rhs_upper[0] += E(vx,kx)*g0(x,t-dt) - A(vx,kx)*g0(x,t)
+            rhs_diag[-1] += G(vx,kx)*g1(x,t-dt) - D(vx,kx)*g1(x,t)# applying BCs here but also in loop
+            rhs_lower[-1] += G(vx,kx)*g1(x,t-dt) - D(vx,kx)*g1(x,t)#
+            
+            rhsY = scipy.sparse.diags([rhs_diag,rhs_lower,rhs_upper],[0,-1,1],format="csr")
+            UY[:,x] = spsolve(lhsY,rhsY.dot(UX[1:-1,x]))
+        
+        U_out = UY
+        alt = 2
+        print('alt: ',alt)
+    elif alt == 2:
+        # in y
+        lhs_lower = A(vy,ky)*np.ones(N)
+        lhs_diag = B(ky)*np.ones(N+1)
+        lhs_upper = D(vy,ky)*np.ones(N)
+        lhsY = scipy.sparse.diags([lhs_diag,lhs_lower,lhs_upper],[0,-1,1],format="csr")
+        lhsY[0,0] += A(vy,kx) # applying BCs
+        lhsY[-1,-1] += D(vy,kx) # applying BCs 
+        
+        rhs_lower = E(vy,ky)*np.ones(N)
+        rhs_diag = F(ky)*np.ones(N+1)
+        rhs_upper = G(vy,ky)*np.ones(N)
+        #rhsY = scipy.sparse.diags([rhs_diag,rhs_lower,rhs_upper],[0,-1,1],format="csr")
+        #rhsY[0,0] += E(vy,ky) # applying BCs
+        #rhsY[-1,-1] += G(vy,ky) # applying BCs
+        for x in range(N+1):
+            # applying boundary conditions strictly, so we are using true values
+            # rather than values at previous time step
+            rhs_diag[0] += E(vx,kx)*g0(x,t-dt) - A(vx,kx)*g0(x,t)# applying BCs here but also in loop
+            rhs_upper[0] += E(vx,kx)*g0(x,t-dt) - A(vx,kx)*g0(x,t)
+            rhs_diag[-1] += G(vx,kx)*g1(x,t-dt) - D(vx,kx)*g1(x,t)# applying BCs here but also in loop
+            rhs_lower[-1] += G(vx,kx)*g1(x,t-dt) - D(vx,kx)*g1(x,t)#
+            
+            rhsY = scipy.sparse.diags([rhs_diag,rhs_lower,rhs_upper],[0,-1,1],format="csr")
+            UY[:,x] = spsolve(lhsY,rhsY.dot(un[1:-1,x]))
+            
+        lhs_lower = A(vx,kx)*np.ones(N)
+        lhs_diag = B(kx)*np.ones(N+1)
+        lhs_upper = D(vx,kx)*np.ones(N)
+        lhsX = scipy.sparse.diags([lhs_diag,lhs_lower,lhs_upper],[0,-1,1],format="csr")
+        lhsX[0,0] += A(vx,kx) # applying BCs
+        lhsX[-1,-1] += D(vx,kx) # applying BCs 
+        
+        rhs_lower = E(vx,kx)*np.ones(N)
+        rhs_diag = F(kx)*np.ones(N+1)
+        rhs_upper = G(vx,kx)*np.ones(N)
+        #rhsX = scipy.sparse.diags([rhs_diag,rhs_lower,rhs_upper],[0,-1,1],format="csr")
+        #rhsX[0,0] += E(vx,kx) # applying BCs here but also in loop
+        #rhsX[-1,-1] += G(vx,kx) # applying BCs here but also in loop
+        
+        for y in range(N+1):  
+            # applying boundary conditions strictly, so we are using true values
+            # rather than values at previous time step
+            rhs_diag[0] += E(vx,kx)*h0(y,t-dt) - A(vx,kx)*h0(y,t)# applying BCs here 
+            rhs_upper[0] += E(vx,kx)*h0(y,t-dt) - A(vx,kx)*h0(y,t)
+            rhs_diag[-1] += G(vx,kx)*h1(y,t-dt) - D(vx,kx)*h1(y,t)# applying BCs here 
+            rhs_lower[-1] += G(vx,kx)*h1(y,t-dt) - D(vx,kx)*h1(y,t)#
+            rhsX = scipy.sparse.diags([rhs_diag,rhs_lower,rhs_upper],[0,-1,1],format="csr")
+            UX[y,:] = spsolve(lhsX,rhsX.dot(UY[y,1:-1]))
+        
+        U_out = UX
+        alt = 1
+        print('alt: ',alt)
+    else:
+        print('Error: not alternating properly.')
+    
+    t2 = perf_counter()
+    dt = t2-t1
+    print('[scipy.sparse.linalg.spsolve] time',('%1.4e'%dt),'(sec)')
+    return U_out, alt
--- pde_2D_adv_diff.py
+++ pde_2D_adv_diff.py
@@ -10,10 +10,10 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import cn_2D_adv_diff as solver
 
-
-def test_cn_convergence():
+def basic_demo():
     '''
     This example tests the cn_2D_diffusion.py solver. 
+    The domain is centered at zero and is square, x,y in (-L/2,L/2)
     '''
     nrm = np.zeros((1))
     hs = np.zeros((1))
@@ -39,6 +39,12 @@ def test_cn_convergence():
     
         def g1(t):
             return true(L,L,np.float(t))
+        
+        def h0(y,t):
+            return true(0,y,t)
+        
+        def h1(y,t):
+            return true(L,y,t)
     
         # define initial condition
         xs = np.linspace(-L/2,L/2,N+1)
@@ -49,7 +55,7 @@ def test_cn_convergence():
         un = u0
         t = 0.0
         for j in range((nt)):
-            un = solver.cn_2D_diffusion(N,dt,L,t,g0,g1,vx,vy,kx,ky,un)
+            un = solver.cn_2D_diffusion(N,dt,L,t,g0,g1,h0,h1,vx,vy,kx,ky,un)
             t += dt
 
             plt.clf()
@@ -68,15 +74,79 @@ def test_cn_convergence():
             ax1.plot_surface(XX,YY,un,cmap=cm.jet,vmin = u0.min(),vmax = u0.max())
             plt.title(['t =', t])
             plt.show()
+
+def test_cn_convergence():
+    '''
+    This example tests the cn_2D_diffusion.py solver. 
+    The domain is square, x,y in (0,L)
+    '''
+    nrm = np.zeros((4))
+    hs = np.zeros((4))
+    for i in range(1,5):
+        #c = int(2*i)
+        N = int(128*2**i) # number of grid points
+        #N = int(64)
+        dt = 1e-6 # time step
+        L = float(1) # length of grid
+        nt = int(5) # number of time steps
+        vx = 0.8
+        vy = 0.8
+        kx = 0.01
+        ky = 0.01
         
-"""
+        def true(x,y,t):
+            return (1.0/(1.0+4.0*t))*np.exp(- (x-0.8*t-0.5)**2/(0.01*(1.0+4.0*t)) - (y-0.8*t-0.5)**2/(0.01*(1.0+4.0*t)))
+
+        # define boundary conditions
+        def g0(x,t):
+            return true(x,0,t)
+        
+        def g1(x,t):
+            return true(x,L,t)
+        
+        def h0(y,t):
+            return true(0,y,t)
+        
+        def h1(y,t):
+            return true(L,y,t)
+
+    
+        # define initial condition
+        xs = np.linspace(0,L,N+1)
+        ys = np.linspace(0,L,N+1)
+        XX, YY = np.meshgrid(xs,ys)
+        u0 = np.exp(-(XX-0.5)**2/0.01 - (YY-0.5)**2/0.01)
+    
+        un = u0
+        t = 0.0
+        alt = 1
+        for j in range((nt)):
+            un,alt = solver.cn_2D_diffusion_alt(N,dt,L,t,g0,g1,h0,h1,vx,vy,kx,ky,un,alt)
+            t += dt
+            plt.clf()
+            fig = plt.figure()
+            ax0 = fig.add_subplot(121, projection='3d')
+            ax1 = fig.add_subplot(122, projection='3d')
+            ax0.set_xlabel('X')
+            ax0.set_ylabel('Y')
+            ax0.set_zlabel('Z')
+            ax1.set_xlabel('X')
+            ax1.set_ylabel('Y')
+            ax1.set_zlabel('Z')
+            ax0.set_zlim(0,1)
+            ax1.set_zlim(0,1)
+            ax0.plot_surface(XX,YY,u0,cmap=cm.jet,vmin = u0.min(),vmax = u0.max())
+            ax1.plot_surface(XX,YY,un,cmap=cm.jet,vmin = u0.min(),vmax = u0.max())
+            plt.title(['t =', t])
+            plt.show()
+        
+        
         hs[i-1] = L/(N-1)
-        # nrm[i-1] = np.linalg.norm(un-true(x,t))
-        nrm[i-1] = np.linalg.norm(un-true(x,t)) * np.sqrt(hs[i-1])
+        #nrm[i-1] = np.linalg.norm(un-true(XX,YY,t)) * np.sqrt(hs[i-1])
+        nrm[i-1] = np.linalg.norm(un-true(XX,YY,t)) * hs[i-1]
         print('Norm',('%1.4e'%nrm[i-1]))
         print('')
-        """
-"""
+        
     print('h',hs)
     print('nrm',nrm)
     fig = plt.figure()
@@ -89,11 +159,12 @@ def test_cn_convergence():
     ax.grid(b='on')
     plt.show()
     print('Slope:',m)
-"""
+
     
     
 if __name__ == '__main__':
-    test_cn_convergence()
+#    test_cn_convergence()
+    basic_demo()
 #    test_cn_example1()
 #    test_cn_comparison()
 #    test_pde_cn()
